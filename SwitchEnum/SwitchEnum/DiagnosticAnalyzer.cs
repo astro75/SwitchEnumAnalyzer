55bdcbd7fc484c18cf848ca06fc768116c136f43
shoter <damian>
2017-08-01 12:00:35 +0200

    Refactor of project.

--- SwitchEnum/SwitchEnum/DiagnosticAnalyzer.cs
+++ SwitchEnum/SwitchEnum/DiagnosticAnalyzer.cs
@@ -20,60 +20,67 @@ namespace SwitchEnum
 		internal const string MessageFormat = "Missing cases:\n{0}";
 		internal const string Category = "Logic";
 
-		internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Error, isEnabledByDefault: true);
-		internal static DiagnosticDescriptor Rule2 = new DiagnosticDescriptor(DiagnosticId, Title2, Title2, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true);
+		public static DiagnosticSeverity DefaultUnreachableSeverity = DiagnosticSeverity.Warning;
+		public static DiagnosticSeverity NotExhaustiveSwitchSeverity = DiagnosticSeverity.Error;
 
-		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule, Rule2);
+		internal static DiagnosticDescriptor NotExhaustiveSwitchRule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, NotExhaustiveSwitchSeverity, isEnabledByDefault: true);
+		internal static DiagnosticDescriptor DefaultUnreachableRule = new DiagnosticDescriptor(DiagnosticId, Title2, Title2, Category, DefaultUnreachableSeverity, isEnabledByDefault: true);
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(NotExhaustiveSwitchRule, DefaultUnreachableRule);
 
 		public override void Initialize(AnalysisContext context)
 		{
 			context.RegisterSyntaxNodeAction(Analyze, SyntaxKind.SwitchStatement);
 		}
 
-		private void Analyze(SyntaxNodeAnalysisContext context)
-		{
-			var switchSyntax = context.Node as SwitchStatementSyntax;
-      if (switchSyntax == null) return;
-			var result = VisitSwitchStatement(context.SemanticModel, switchSyntax, context.CancellationToken);
-		  if (result == null) return;
-		  var hasDefault = result.Item2;
-		  var defaultIsThrow = result.Item3;
-      if (result.Item1.Count > 0) {
-		    if (!hasDefault || defaultIsThrow) {
-		      var diagnostic = Diagnostic.Create(Rule, switchSyntax.Expression.GetLocation(), string.Join("\n", result.Item1));
-          context.ReportDiagnostic(diagnostic);
-        }
-      }
-		  else {
-        if (hasDefault && !defaultIsThrow) {
-          var diagnostic = Diagnostic.Create(Rule2, switchSyntax.Expression.GetLocation());
-          context.ReportDiagnostic(diagnostic);
-        }
-      }
-		}
+        private void Analyze(SyntaxNodeAnalysisContext context)
+        {
+            var switchSyntax = context.Node as SwitchStatementSyntax;
+            if (switchSyntax == null) return;
+            SwitchInformation information = GetInformationAboutSwitch(context.SemanticModel, switchSyntax, context.CancellationToken);
 
-		Tuple<List<string>,bool,bool> VisitSwitchStatement(SemanticModel model, SwitchStatementSyntax node, CancellationToken ct)
+            if (information is SwitchInformation)
+            {
+                if (information.NotExhaustiveSwitch)
+                {
+                    var diagnostic = Diagnostic.Create(NotExhaustiveSwitchRule, switchSyntax.Expression.GetLocation(), string.Join("\n", information.NotFoundSymbolNames));
+                    context.ReportDiagnostic(diagnostic);
+                }
+                else if(information.UnreachableDefault)
+                {
+                    var diagnostic = Diagnostic.Create(DefaultUnreachableRule, switchSyntax.Expression.GetLocation());
+                    context.ReportDiagnostic(diagnostic);
+                }
+            }
+        }
+		
+
+        SwitchInformation GetInformationAboutSwitch(SemanticModel model, SwitchStatementSyntax node, CancellationToken ct)
 		{
-			try {
-				var type = model.GetTypeInfo(node.Expression, ct).Type;
-				if (type.TypeKind == TypeKind.Enum) {
-					                                        // Exclude ctor
+            try
+			{
+                SwitchInformation info = new SwitchInformation();
+
+                var type = model.GetTypeInfo(node.Expression, ct).Type;
+				if (type.TypeKind == TypeKind.Enum)
+				{
+					// Exclude ctor
 					var members = type.GetMembers().Where(m => m.Kind == SymbolKind.Field);
 					var defaults = node.Sections.SelectMany(s => s.Labels).OfType<DefaultSwitchLabelSyntax>().ToArray();
-				  var hasDefault = defaults.Any();
-				  var defaultIsThrow = false;
-					if (hasDefault) {
-					  var d = defaults.First();
-					  var first = ((SwitchSectionSyntax) d.Parent).Statements.FirstOrDefault();
-					  defaultIsThrow = first is ThrowStatementSyntax;
+					info.HasDefault = defaults.Any();
+					if (info.HasDefault)
+					{
+						var d = defaults.First();
+						var first = ((SwitchSectionSyntax)d.Parent).Statements.FirstOrDefault();
+						info.DefaultIsThrow = first is ThrowStatementSyntax;
 					}
-          var symbols = node.Sections.SelectMany(
-              s => s.Labels.OfType<CaseSwitchLabelSyntax>().Select(l => model.GetSymbolInfo(l.Value, ct).Symbol)).ToArray();
-          var notFound = members.Where(m => !symbols.Any(s => Equals(s, m))).Select(m => m.Name).ToList();
-          return Tuple.Create(notFound, hasDefault, defaultIsThrow);
-        }
+					var symbols = node.Sections.SelectMany(
+						s => s.Labels.OfType<CaseSwitchLabelSyntax>().Select(l => model.GetSymbolInfo(l.Value, ct).Symbol)).ToArray();
+					info.NotFoundSymbolNames = members.Where(m => !symbols.Any(s => Equals(s, m))).Select(m => m.Name).ToList();
+				}
 			}
-			catch (Exception) {
+			catch (Exception)
+			{
 				// ignored
 			}
 			return null;
---
+++ SwitchEnum/SwitchEnum/SwitchInformation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchEnum
+{
+    public class SwitchInformation
+    {
+        public List<string> NotFoundSymbolNames { get; set; } = new List<string>();
+        public bool HasDefault { get; set; }
+        public bool DefaultIsThrow { get; set; }
+        public bool UnreachableDefault => HasDefault && NotFoundSymbolNames.Count == 0 && !DefaultIsThrow;
+        public bool NotExhaustiveSwitch => NotFoundSymbolNames.Any() && (HasDefault || DefaultIsThrow);
+    }
+}
